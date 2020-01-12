3530b08b575a44fa5dbc1aa190b7b3b4e5330044
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-12 21:19:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,16 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  
+   
+  
+  attr_accessor :author, :page_count, :genre, :genres
   attr_reader :title
 
   def initialize(title)
     @title = title
+    @genre = genre
+    Book::GENRES
+    @genres = []
+    @genres << @genre 
   end
 
   def turn_page
